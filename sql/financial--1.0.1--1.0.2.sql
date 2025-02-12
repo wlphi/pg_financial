7ca5b5ae95b454ffc73bf976336842d1bd6954c2
wlphi <122044808+wlphi@users.noreply.github.com>
2025-02-12 16:40:58 +0100

    Update financial--1.0.1--1.0.2.sql

--- sql/financial--1.0.1--1.0.2.sql
+++ sql/financial--1.0.1--1.0.2.sql
@@ -1,12 +1,1 @@
--- Aggregate function for TWR calculation
-CREATE FUNCTION twr_tstz_transfn(internal, float8, timestamptz)
-RETURNS internal IMMUTABLE LANGUAGE C AS 'financial';
-
-CREATE FUNCTION twr_tstz_finalfn(internal)
-RETURNS float8 IMMUTABLE LANGUAGE C AS 'financial';
-
-CREATE AGGREGATE twr (float8, timestamptz) (
-    SFUNC = twr_tstz_transfn,
-    STYPE = internal,
-    FINALFUNC = twr_tstz_finalfn
-);
+--

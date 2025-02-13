355f5916b90685302f07029cfe171f78053536d5
wlphi <122044808+wlphi@users.noreply.github.com>
2025-02-13 10:52:29 +0100

    Update financial.sql

--- sql/financial.sql
+++ sql/financial.sql
@@ -20,3 +20,20 @@ CREATE AGGREGATE xirr (float8, timestamptz, float8) (
     STYPE = internal,
     FINALFUNC = xirr_tstz_finalfn
 );
+
+-- twr(amount, value, timestamp)
+CREATE FUNCTION twr_tstz_transfn(internal, double precision, double precision, timestamp with time zone)
+RETURNS internal
+AS 'MODULE_PATHNAME', 'twr_tstz_transfn'
+LANGUAGE C IMMUTABLE;
+
+CREATE FUNCTION twr_tstz_finalfn(internal)
+RETURNS double precision
+AS 'MODULE_PATHNAME', 'twr_tstz_finalfn'
+LANGUAGE C IMMUTABLE;
+
+CREATE AGGREGATE twr(amount double precision, value double precision, "time" timestamp with time zone) (
+    SFUNC = twr_tstz_transfn,
+    STYPE = internal,
+    FINALFUNC = twr_tstz_finalfn
+);

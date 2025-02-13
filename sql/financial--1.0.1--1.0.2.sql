2484ce22c980cb337f3ba50ba2035cde775acea2
wlphi <122044808+wlphi@users.noreply.github.com>
2025-02-13 10:44:48 +0100

    Update financial--1.0.1--1.0.2.sql

--- sql/financial--1.0.1--1.0.2.sql
+++ sql/financial--1.0.1--1.0.2.sql
@@ -1,1 +1,15 @@
---
+CREATE OR REPLACE FUNCTION twr_tstz_transfn(internal, double precision, double precision, timestamp with time zone)
+RETURNS internal
+AS 'MODULE_PATHNAME', 'twr_tstz_transfn'
+LANGUAGE C IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION twr_tstz_finalfn(internal)
+RETURNS double precision
+AS 'MODULE_PATHNAME', 'twr_tstz_finalfn'
+LANGUAGE C IMMUTABLE;
+
+CREATE AGGREGATE twr(amount double precision, value double precision, "time" timestamp with time zone) (
+    SFUNC = twr_tstz_transfn,
+    STYPE = internal,
+    FINALFUNC = twr_tstz_finalfn
+);
